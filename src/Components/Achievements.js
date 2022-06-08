b99279532038704f589fd60102d54175e46e7e95
gagan mahesh <gagmahes@cisco.com>
2022-06-08 16:38:10 +0530

    added 'under construction' message for mobile /achievements ui

--- src/Components/Achievements.js
+++ src/Components/Achievements.js
@@ -4,45 +4,51 @@ import "../Stylings/Achievements.css";
 import Carousel from "react-bootstrap/Carousel";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { achievements } from "../Contents/achievements";
+import DetectRTC from "detectrtc";
 
 const Achievements = () => {
   return (
     <div className="achievements-page">
-      <div className="container">
-        <Carousel fade>
-          {achievements.map((item, index) => {
-            return (
-              <Carousel.Item>
-                <div className="card" key={index}>
-                  <div className="card-header">{item.header}</div>
-                  <ul className="card-description-list">
-                    {item.description.map((i, ind) => {
-                      return (
-                        <li className="card-description" key={ind}>
-                          {i.desc}
-                        </li>
-                      );
-                    })}
-                    {item.certificates?.map((i, ind) => {
-                      return (
-                        <Link
-                          className="certificate-button"
-                          to={i.file}
-                          target="_blank"
-                          key={ind}
-                          download
-                        >
-                          {i.caption}
-                        </Link>
-                      );
-                    })}
-                  </ul>
-                </div>
-              </Carousel.Item>
-            );
-          })}
-        </Carousel>
-      </div>
+      {!DetectRTC.isMobileDevice && (
+        <div className="container">
+          <Carousel fade>
+            {achievements.map((item, index) => {
+              return (
+                <Carousel.Item>
+                  <div className="card" key={index}>
+                    <div className="card-header">{item.header}</div>
+                    <ul className="card-description-list">
+                      {item.description.map((i, ind) => {
+                        return (
+                          <li className="card-description" key={ind}>
+                            {i.desc}
+                          </li>
+                        );
+                      })}
+                      {item.certificates?.map((i, ind) => {
+                        return (
+                          <Link
+                            className="certificate-button"
+                            to={i.file}
+                            target="_blank"
+                            key={ind}
+                            download
+                          >
+                            {i.caption}
+                          </Link>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </Carousel.Item>
+              );
+            })}
+          </Carousel>
+        </div>
+      )}
+      {DetectRTC.isMobileDevice && (
+        <h1 style={{ color: "white" }}>Page being built!</h1>
+      )}
     </div>
   );
 };
